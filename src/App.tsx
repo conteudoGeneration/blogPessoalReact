b0d5eb3b03b0b351a1e99f8792512ee461b915ef
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-10-01 09:06:54 -0300

    blog create project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+   <Home />
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" width="500" height="500"/>
+        </>
+    );
+}
+
+export default Home;
