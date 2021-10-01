f8665e39dc5b720d65b25b78cf75b802f89ca841
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-10-01 09:10:02 -0300

    3-blogPessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
 
 
 function App() {
   return (
-   <Home />
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Button, Box, Paper} from '@material-ui/core';
 import './Home.css';
 
 function Home(){
     return (
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
+           <Paper>
+               <Box p={2}>
+                   <Box display="flex" justifyContent="center">
+                       <h1>Título</h1>
+                   </Box>
+                   <img src="https://schultzfamilyfoundation.org/wp-content/uploads/2019/07/generation-logo.png" alt="" style={{width: "100%", height: "100%"}}/>
+                   <Box display="flex" justifyContent="center" p={2}>
+                       <Button variant="contained" color="primary">Texto 1</Button>
+                       <Button variant="contained" color="secondary">Texto 2</Button>
+                    </Box>
+               </Box>
+           </Paper>
         </>
     );
 }
