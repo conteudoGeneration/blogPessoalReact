54329ee1e5ed266e41a510bc5ac851752758c6d1
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-10-01 09:23:10 -0300

    11-blogPessoal

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
-import { api } from '../../services/Service';
+import { login } from '../../services/Service';
 import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
@@ -35,8 +35,7 @@ function Login() {
         async function onSubmit(e: ChangeEvent<HTMLFormElement>){
             e.preventDefault();
             try{
-                const resposta = await api.post(`/usuarios/logar`, userLogin)
-                setToken(resposta.data.token)
+                await login(`/usuarios/logar`, userLogin, setToken)
 
                 alert('Usuário logado com sucesso!');
             }catch(error){
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,7 +4,12 @@ export const api = axios.create({
     baseURL: 'https://bloggeneration.herokuapp.com'
 })
 
-  export const login = async(url: any,dados: any,setDado: any) => { 
-    const resposta = await api.post(url,dados)
-    setDado(resposta.data)
-  }
+    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data)
+    }
+
+    export const login = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data.token)
+    }
