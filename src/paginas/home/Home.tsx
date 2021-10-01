4200c8d7fb6597091607ce76f257ff757677e8b1
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-10-01 09:08:10 -0300

    2-blogPessoal

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,8 +4,8 @@ import './Home.css';
 function Home(){
     return (
         <>
-            <h1>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" width="500" height="500"/>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
         </>
     );
 }
