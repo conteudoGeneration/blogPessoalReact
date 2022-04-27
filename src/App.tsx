10e8cb095e8f4be8ba83fa29a2b1586277d783d9
LucasCapSilva <57732522+LucasCapSilva@users.noreply.github.com>
2022-04-27 13:59:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login  />} />
 
         <Route path="/home" element={<Home />} />
 
