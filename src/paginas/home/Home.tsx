1c6fe7483ed64b3f73301f827d5ef1d70102f28d
unknown <lcapelotto@gmail.com>
2022-11-17 15:20:33 -0300

    1.0

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
 import './Home.css';
 
 function Home() {
